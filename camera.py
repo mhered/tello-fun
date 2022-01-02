af35801227f7aa8c927371e6ccea6b2073104d3b
Manuel Heredia <manolo.heredia@gmail.com>
2022-01-03 00:12:05 +0100

    Display streaming mirroredand resized 360x240

--- camera.py
+++ camera.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from djitellopy import tello
-from time import sleep
+import cv2
 
 drone = tello.Tello()
 drone.connect()
@@ -10,3 +10,20 @@ drone.connect()
 # ./myvenv/lib/python3.8/site-packages/djitellopy/tello.py
 
 print(drone.get_battery())
+
+drone.streamon()
+
+while True:
+    # get frame
+    frame = drone.get_frame_read().frame
+    # resize
+    frame_xs = cv2.resize(frame, (360, 240))
+    # mirror for display
+    frame_xs_flipped = cv2.flip(frame_xs, 1)
+
+    # show images
+    cv2.imshow("Frame", frame_xs_flipped)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+drone.streamoff()
+cv2.destroyAllWindows()
