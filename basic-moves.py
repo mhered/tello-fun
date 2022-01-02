3188333a0a5667bd957c864aac2eb6cd0a2b3963
Manuel Heredia <manolo.heredia@gmail.com>
2022-01-02 23:17:18 +0100

    change sequenceof basic moves

--- basic-moves.py
+++ basic-moves.py
@@ -12,20 +12,17 @@ drone.connect()
 drone.takeoff()
 
 # (L/R, fwd/bk, up/down, anti/clockwise yaw) each in -/+100%
-drone.send_rc_control(0, 30, 0, 0)
-sleep(2)
-
 drone.send_rc_control(0, 0, 30, 0)
 sleep(2)
+
+drone.send_rc_control(0, 0, 0, 80)
+sleep(3)
 
 drone.send_rc_control(0, 0, 0, 0)
 sleep(1)
 
-drone.send_rc_control(0, 0, -30, 0)
-sleep(2)
-
-drone.send_rc_control(0, -30, 0, 0)
-sleep(2)
+drone.send_rc_control(0, 0, 0, -80)
+sleep(3)
 
 drone.send_rc_control(0, 0, 0, 0)
 sleep(1)
